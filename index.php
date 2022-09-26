b1b30c8fcd14043b00d8c2ca2a68a3d4a40c0ea8
Eduardo <eduardo@javapple.io>
2022-09-26 15:00:25 -0500

    Se agrego version a app.js

--- index.php
+++ index.php
@@ -152,7 +152,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/stats.js/r11/Stats.js"></script>
     <script src='https://code.jquery.com/jquery-3.6.0.min.js' integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin='anonymous'></script>
     <script src="./scripts/map.min.js"></script>
-    <script type="module" src="./scripts/app.js"></script>
+    <script type="module" src="./scripts/app.js?ver=<?php echo date("ymd-Gi", filemtime("./scripts/app.js")) ?>"></script>
     <script>
         $(document).ready(function(e) {
             $('img[usemap]').rwdImageMaps();
