fd9e9a9c2b8493ddd6ad01261c0fb593b85fb086
Eduardo <eduardo@javapple.io>
2022-09-27 22:30:51 -0500

    Prueba animacion canva en mobil

--- scripts/app.js
+++ scripts/app.js
@@ -73,11 +73,28 @@
 		
 	}
 
-	function animate() {
-		requestAnimationFrame(animate);
-		render();
-		// stats.update();
-	}
+	function myFunction(x) {
+		if (x.matches) { // If media query matches
+			function animate() {
+				camera.position.x += 0.005;
+				requestAnimationFrame(animate);
+				render();
+				// stats.update();
+			}
+		} else {
+			function animate() {
+				requestAnimationFrame(animate);
+				render();
+				// stats.update();
+			}
+		}
+	  }
+	  
+	  var x = window.matchMedia("(max-width: 700px)")
+	  myFunction(x) // Call listener function at run time
+	  x.addListener(myFunction) // Attach listener function on state changes
+
+	
 
 
 	function render() {
